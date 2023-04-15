a6434ade0797f8a1097401e2d7bb09f3dd8d7f6d
Sebas <130624090+SebastianJimenezAl99@users.noreply.github.com>
2023-04-15 11:31:10 -0500

    Hola

--- src/main/java/com/example/controlador/ProfesorControlador.java
+++ src/main/java/com/example/controlador/ProfesorControlador.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.hibernate.engine.jdbc.spi.SqlExceptionHelper;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
